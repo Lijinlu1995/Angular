fd35a38606243f1f7f1855fcf4e7eb9c807b871d
Lijinlu1995 <lijinlu1995@foxmail.com>
2018-10-08 08:33:25 +0800

    add angular page and add sample index

--- todomvc-app/js/app.js
+++ todomvc-app/js/app.js
@@ -5,6 +5,7 @@
   // 1. 为应用程序创建一个模块，用来管理界面的结构
   var myApp = angular.module('app', ['ngRoute', 'app.controllers.main']);
 
+
   // 路由配置
   myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider
